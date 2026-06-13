3112f5e62664106512f03b65f68af5d6bec8c0b5
M.jamal Alhneidi <jamal.alhneidi@gmail.com>
2026-06-13 14:48:39 +0200

    improved plotting in mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -41,11 +41,35 @@ def mergeSort(list_to_sort_by_merge):
 
 import matplotlib.pyplot as plt
 
+""" 
+- We print two plots next to each other: one before sorting and one after sorting.
+- Since the values are discrete, it makes more sense to display them as bars.
+- We also add the values on top of the bars to make it easier to track specific values.
+- We use different colors for the two plots to make it easier to distinguish them.
+"""
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 5))
+
+for ax in [ax1, ax2]:
+    ax.set_xticks(x)
+    ax.set_xlabel("Index")
+    ax.set_ylabel("Value")
+    ax.grid(True, axis="y", alpha=0.3)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+
+# Plotting the original list before sorting
+bars1 = ax1.bar(x, my_list, color="firebrick")
+ax1.set_title("Before sorting", color="firebrick")
+ax1.bar_label(bars1, padding=3, color="firebrick")
 mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+# Plotting the sorted list after applying merge sort
+bars2 = ax2.bar(x, my_list, color="green")
+ax2.set_title("After sorting", color="green")
+ax2.bar_label(bars2, padding=3, color="green")
+plt.tight_layout()
 plt.show()
